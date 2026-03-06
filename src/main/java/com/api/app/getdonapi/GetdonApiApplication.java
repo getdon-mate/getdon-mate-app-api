6f2f2caba1db70ff3f666426cd4f3dcf31617b1d
byj <yeongjun1324@gmail.com>
2026-03-06 18:47:38 +0900

    init : created project

---
+++ src/main/java/com/api/app/getdonapi/GetdonApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.app.getdonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetdonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetdonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/app/getdonapi/GetdonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.app.getdonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetdonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
